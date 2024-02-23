dcfb95d0940f5ce111b7a61312db3b6247b46a46
Russel Roy <153712847+royroy7@users.noreply.github.com>
2024-02-23 11:12:40 +0800

    init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(7)->create();
+
+        Listing::factory(7)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Laravel Junior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@gmail.com',
+        //     'website' => 'https:://www.acme.com',
+        //     'description' => 'Lorem Ipsum'
+        // ]);
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@gmail.com',
+        //     'website' => 'https:://www.acme.com',
+        //     'description' => 'Lorem Ipsum'
+        // ]);
+    }
+}
